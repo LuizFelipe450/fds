555d830ffce50f059a53cefb5b88f42479cf4244
LuizFelipe450 <77294904+LuizFelipe450@users.noreply.github.com>
2021-01-12 19:05:20 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,91 @@
+🔥MENU🔥 = (prefix) => {
+	return `> *      (FIGURINHAS)      * <
+command : *${prefix}sticker* or *${prefix}stiker*
+COMANDO:/sticker
+DESC:converter imagem / gif / vídeo em adesivo
+uso:basta enviar o comando
+command : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+COMANDO:/sticker nobg
+DESC:converter imagem em adesivo removendo o fundo
+uso:responda a figurinha com o comando
+command : *${prefix}toimg*
+desc : convert sticker to image
+usage : reply sticker\n
+command : *${prefix}tsticker* or *${prefix}tstiker*
+desc : convert text to sticker
+usage : *${prefix}tsticker text in here*\n
+
+> *     Meme     * <
+command : *${prefix}meme*
+desc: imagens aleatórias de meme [inglês]
+uso: basta enviar o comando \meme
+command : *${prefix}memeindo*
+desc: imagens meme aleatórias [indo]
+uso: basta enviar o comando \memeindo
+
+> *     FUNNY     * <
+command : *${prefix}loli*
+desc: imagens aleatórias de loli
+uso: basta enviar o comando \loli
+command : *${prefix}loli18*
+desc : imagems +18 de lolis
+uso: basta enviar o comando/loli18
+
+Nota: so funciona no pv
+
+command : *${prefix}simi*
+desc : sua mensagem será respondida por simi
+usage :  /simi [seu texto]
+command : *${prefix}wait*
+desc : Saiba sobre o anime [Que anime é]
+uso: Envie imagem do anime com o comando
+
+> *     🔥 GRUPO 🔥     * <
+    🔥 COMANDOS ADMINS 🔥
+command : *${prefix}add*
+desc : adicionar membro ao grupo
+uso: * $ {prefix} /add 55xxxxxxxx
+nota : só pode ser usado quando o bot for torna administrador, e aquele que envia o o comando é admin! \add 55xxxxxxxx
+command : *${prefix}kick*
+desc : kick membros do grupo
+uso: * $ {prefix} /kick @ tag do membro * 
+observação: só pode ser usado quando o bot se for admin e quem envia o comando é admin! \kick @teg do membro
+command : *${prefix}promote*
+desc : tornar o membro do grupo como um grupo admin
+uso: * $ {prefix} promova @ tagmember * \ n observação: só pode ser usado quando o bot se for admin e quem envia o comando é admin! \promote @tag do membro
+command : *${prefix}demote*
+desc: tornar o administrador do grupo como membro do grupo
+uso: * $ {prefix} demote @ tagmember * observação: só pode ser usado quando o bot se fot admin e quem envia o comando é admin! \demote @teg do membro
+command : *${prefix}linkgroup*
+desc: pegue o link do grupo
+uso: basta enviar o comando
+observação: só pode ser usado quando o bot for admin e quem envia o comando é admin! \linkgroup
+command : *${prefix}leave*
+desc: Faça o bot sair do grupo
+uso: basta enviar o comando
+nota: só pode ser usado por administradores de grupo e proprietário do bot \leave
+command : *${prefix}tagall*
+desc: marca todos os membros do grupo, incluindo administradores também
+uso: basta enviar o comando
+nota: este comando pode ser usado se você for um administrador de grupo \tagall
+command : *${prefix}simih*
+desc: ativa o modo simi no grupo
+uso: * $ {prefix} simih 1 * para ativar o modo simi e * $ {prefix} simih 0 * para desativar o modo simi
+nota: este comando pode ser usado se você for um administrador de grupo \simih
+}
+
+exports.help = help
+
+------------------------------------------CRIADORES DO BOT 
+
+-Luiz
+
+ctt:http://wa.me/+551199999-3741
+
+insta:to sem kk
+
+-𝐷𝑂𝐵𝐵𝑌♱᭄ 
+
+ctt:http://wa.me/+5511911219615
+
+inta:tbm ta sem;-;
